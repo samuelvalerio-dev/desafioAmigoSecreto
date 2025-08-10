cd30a72c31a0c11407b46f58537bc31bc1d542b1
Samuel Valério <samuelvalerio.filho@gmail.com>
2025-08-10 19:25:09 -0300

    Primeiro commit - desafio amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let input = document.getElementById('amigo');
+    let novoAmigo = input.value.trim();
+
+    if (novoAmigo === '') {
+        alert('Digite um nome válido');
+        return;
+    }
+
+    amigos.push(novoAmigo);
+    input.value = '';
+    atualizarLista();
+}
+
+function atualizarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    amigos.forEach(amigo => {
+        let li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('Adicione amigos antes de sortear!');
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoEscolhido = amigos[indiceAleatorio];
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<li>O amigo secreto sorteado é: ${amigoEscolhido}</li>`;
+}
